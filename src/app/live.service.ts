7c68f69cba6301348e4b66205df2224f252dbf0b
Blaise Hala <blaisehala@gmail.com>
2022-04-18 02:32:17 +0300

    user fetching logic added

--- src/app/live.service.ts
+++ src/app/live.service.ts
@@ -7,15 +7,21 @@ import { Observable } from "rxjs";
   providedIn: 'root'
 })
 export class LiveService {
-  prop_link = " https://api.github.com/ "
-  constructor(private http: HttpClient) { }  
-
-  getUsers():Observable{ 
-
-   }
-
-  getRepos(){
-    console.log('test')
+  getUser() {
+    throw new Error ('Method not immplmented');
   }
 
+  propLink = " https://api.github.com/ "
+
+  user = 'Maltilda Nyaboke';
+  userLink:string= `https://api.github.com/users${this.user}`
+  repoLink:string=  `https://api.github.com/users/${this.user}/repos`
+  
+  constructor(private http: HttpClient) { } 
+  getUsers():Observable<any>{
+    return this.http.get<any>(this.userLink)
+  } 
+   getRepos():Observable<any>{
+     return this.http.get<any>(this.repoLink)
+   }
 }
