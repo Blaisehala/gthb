2b216742a3561b5cbb24bbc7c1b074863edb6ff9
Blaise Hala <blaisehala@gmail.com>
2022-04-17 23:35:36 +0300

    addition of files

--- src/app/live.service.ts
+++ src/app/live.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class LiveService {
+  prop_link = " https://api.github.com/ "
+  constructor(private http: HttpClient) { }  
 
-  constructor() { }
+  getUsers():Observable{ 
+
+   }
+
+  getRepos(){
+    console.log('test')
+  }
+
 }
