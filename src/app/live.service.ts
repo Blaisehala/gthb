8013db44436357c30c56ad15e199a499361933a3
Blaise Hala <blaisehala@gmail.com>
2022-04-18 10:51:44 +0300

    addition of asynchronous function

--- src/app/live.service.ts
+++ src/app/live.service.ts
@@ -1,21 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
-
+import { lastValueFrom, Observable } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LiveService {
-  user = 'Blaisehala'
+  userName = 'Blaisehala'
  
 
-  propLink =  `https://api.github.com/users/${this.user}`
+  propLink =  `https://api.github.com/`
 
  
   constructor(private http: HttpClient) { } 
-  getUsers():Observable<any>{
-    return this.http.get<any>(this.propLink)
+ async getUsers(){
+   const papa = this.http.get<any>(`${this.propLink}/users/${this.userName}`)
+    return await lastValueFrom (papa).then((response)=>response)
   } 
 
 }
+
+
+
+
+//promise ---kind of waiting 
