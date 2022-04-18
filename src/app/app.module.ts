50bcd50fed8b1e3bc396fcad6eb26887fddbd1c9
Blaise Hala <blaisehala@gmail.com>
2022-04-18 12:53:03 +0300

    creation of form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
+import { SearchformComponent } from './searchform/searchform.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent
+    UsersComponent,
+    SearchformComponent
   ],
   imports: [
     BrowserModule,
