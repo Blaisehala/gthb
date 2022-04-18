d9b527bea55aca037012b797463b33d8104af241
Blaise Hala <blaisehala@gmail.com>
2022-04-18 11:19:13 +0300

    creates funtion to fetch repos

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,21 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { LiveService } from '../live.service';
 
+
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  userName = 'Blaisehala'
 user!:any;
+repo!:any;
   constructor(private liveService:LiveService ) { }
 
+  repoFetch(userName:string){
+    this.liveService.getRepo(userName).subscribe((repo)=>{
+      this.repo=repo
+      console.log(repo);
+    })
+  }
+
+  
+
   ngOnInit(): void {
-      this.liveService.getUsers().subscribe(
-        data => 
-        {this.user =data
-        console.log(this.user);}
-      )
+    this.repoFetch(this.userName)
+  //     this.liveService.getUsers().subscribe(
+  //       data => 
+  //       {this.user =data
+  //       console.log(this.user);}
+  //     )
   }
 
 }
